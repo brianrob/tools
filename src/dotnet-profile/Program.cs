ccfc8428d80026421f1902d6b6faea54de4482d9
Brian Robbins <brianrob@microsoft.com>
2018-08-31 11:05:19 -0700

    Initial commit of dotnet-profile.

---
+++ src/dotnet-profile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
